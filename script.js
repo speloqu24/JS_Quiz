9b8feaa6bcabfba25c8285acb306e754cd636d65
Speloquin <speloqu24@gmail.com>
2020-10-13 17:49:13 -0600

    First Psuedo Code

---
+++ script.js
@@ -0,0 +1,71 @@
+//Psuedo 
+
+// Needs the page to already load html
+
+// BODY VARIABLE
+// var body = document.body;
+
+//CREATE ELEMENTS
+// var h1TimerText = document.createElement.("h1")
+// var quizRulesText = document.createElement.("p")
+
+
+// SET TEXT CONTENT
+// Heading text - "Welcome to your Bootcamp Code Quiz"
+    // h1TimerText.textContent = "Welcome to your BootCamp Code Quiz"
+
+// Paragraph text -  "This quiz is going to test your fundamental understanding of JavaScript. 
+// You have 90 seconds to answer all of the questions correctly....""
+    // quizRulesText.textContent = ""This quiz is going to test your fundamental understanding of JavaScript. 
+// You have 90 seconds to answer all of the questions correctly. If a question is 
+// answered in correctly 5 seconds will be removed from the timer, limiting your time
+// even more. When the timer reaches "0" your score will be displayed. Goodluck!""
+
+
+
+// SET ELEMENT ATTRIBUTES
+// Heading text - "Welcome to your Bootcamp Code Quiz" - Black 
+
+
+
+
+
+// "Start Quiz Button" > click button 
+// Because there's a click there will be an "eventListener"
+
+// Create score variables
+// var score = 0; (start at 0)
+// for loop to increase score by 1 (++) if correct 
+
+// When "Start Quiz Button" is clicked the Heading text and Paragraph text are replaced
+// with the first question. 
+// 90 Timer starts 
+
+// var secondsleft = 90;
+// secondsLeft --; (decrease by 1)
+// second statement needs to be , 1000); - this will run the clock every second
+
+// first question 
+
+// If statement for question 
+// user clicks on their selection = var userGuess
+// function setTimer (){
+    // var timerInterval = setInterval (function () {
+    // secondsLeft --; 
+    //        .textContent = "Timer: " + secondsLeft }
+// if (userGuess == true) {
+    // trigger function for next question 
+
+
+
+
+
+// Global Variables
+// var start time
+// var time left
+
+// Declare Functions 
+
+// function runTime () {}
+// function askQuestions () {}
+
