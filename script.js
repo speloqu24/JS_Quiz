09777f84c01672f86aff05f89d882dd7bef92d16
Speloquin <speloqu24@gmail.com>
2020-10-18 22:37:36 -0600

    formatting

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// TEST JAVA
-// alert("this is working");
-
 // VARIABLE DECLARATION : Storing variables in global memory
 var mainDiv = document.getElementById("main-div");
 var startQuiz = document.getElementById("startQuiz");
@@ -23,24 +20,24 @@ var questionsArray = [
     question: "What does DOM stand for?",
     options: [
       "Document Object Model",
-      "this answer is wrong",
-      "DoCT",
-      "Dive into modes",
+      "Document Object Management ",
+      "Document Object Modes",
+      "Document Object Manager",
     ],
     correct: "Document Object Model",
   },
   {
-    question: "What do we use to store global variables?",
+    question: "What do we use to declare variables??",
     options: ["gl", "var", "gv", "vari"],
     correct: "var",
   },
   {
     question: "When does a function run in JS?",
-    options: ["{}", "#", "[]", "()"],
-    correct: "()",
+    options: ["{ }", "#", "[ ]", "( )"],
+    correct: "( )",
   },
   {
-    question: "How is JS added to HTML?",
+    question: "How is JS inserted into HTML?",
     options: ["<scripthtml>", "<js>", "<javascript>", "<script>"],
     correct: "<script>",
   },
@@ -50,13 +47,13 @@ var questionsArray = [
       "counter, change, index",
       "counter, condition, index",
       "change, counter, iterator ",
-      "counter, condition, iterator",
+      "counter, condition, iteration",
     ],
-    correct: "counter, condition, iterator",
+    correct: "counter, condition, iteration",
   },
   {
     question:
-      "An 'alert' pops up, and text input from the user is requested?. What is that called?",
+      "An 'alert' pops up and text input from the user is requested? What is this called?",
     options: ["prompt", "confirm", "bolean", "variable"],
     correct: "prompt",
   },
@@ -72,51 +69,51 @@ var questionsArray = [
   },
 ];
 
-// Hide elements
+// HIDE elements
 document.getElementById("questionDisplay").style.visibility = "hidden";
 document.getElementById("score-div").style.visibility = "hidden";
 document.getElementById("endScreen").style.visibility = "hidden";
 
-// START QUIZ WITH TIMER FUNCTION
+// LISTENING for the onclick to start the quiz
+startQuiz.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("main-div").style.visibility = "hidden";
+    document.getElementById("questionDisplay").style.visibility = "visible";
+    timerDisplay();
+    triggerQuestion();
+});
+
+// START quiz & trigger timer
 function timerDisplay() {
   var timeInterval = setInterval(function () {
     timerDiv.textContent = "Timer: " + secondsLeft;
     secondsLeft--;
     if (secondsLeft === 0 && questionsArray.length === 0) {
-        alert ("Timer Ran out!")
-        window.location = '/';
+      alert("Timer Ran out!");
+      window.location = "/";
       clearInterval(timeInterval);
     }
   }, 1000);
 }
-// LISTENING for the onclick to start the quiz
-startQuiz.addEventListener("click", function (event) {
-  event.preventDefault();
-  document.getElementById("main-div").style.visibility = "hidden";
-  document.getElementById("questionDisplay").style.visibility = "visible";
-  timerDisplay();
-  triggerQuestion();
-});
-
-// // LOCAL STORAGE
+// LOCAL STORAGE for username, and error check for input
 startQuiz.addEventListener("click", function (event) {
   event.preventDefault();
 
-  var user = userInput.value.trim()
-  localStorage.setItem("user",(user));
+  var user = userInput.value.trim();
+  localStorage.setItem("user", user);
 
   var prevUser = localStorage.getItem("user");
   userNameDisplay.textContent = prevUser.name;
 
   console.log(user);
-  
-  if (user.name === "") {
-    alert("Name field CANNOT be blank");
-    window.location.reload();
+
+  if (user === "") {
+      alert ("cannot have no name");
+      window.location.reload()
   }
 });
 
-// TRIGGER QUESTION FUNCTION - Displays the question and listens for an onclick
+// TRIGGER question function - Displays the question and listens for an onclick
 function triggerQuestion() {
   theAsk.textContent = questionsArray[next].question;
 
@@ -130,29 +127,24 @@ function triggerQuestion() {
   answer4Btn.addEventListener("click", questionIterate);
 }
 
-// QUESTION ITERATE FUNCTION - Iterates through the question index and listens for an event.
+// QUESTION ITERATE & LOCAL STORAGE for userScore - Iterates through the question index and listens for an event.
 function questionIterate(event) {
   event.preventDefault();
-
   var answer = event.target.textContent;
   checkAnswer(answer);
   console.log(answer);
   next++;
   if (next >= questionsArray.length) {
-
-  localStorage.setItem("userScore", score);
-  var prevScore = localStorage.getItem("userScore");
-  scoreCount.textContent = prevScore     
-
+    localStorage.setItem("userScore", score);
+    var prevScore = localStorage.getItem("userScore");
+    scoreCount.textContent = prevScore;
     endScreen();
     return;
   }
-
   triggerQuestion();
 }
 
-// CHECK ANSWER and SCORE Function - Listens for the answer event and compares it to the correct answer
-// if it's correct score increase by 1 if it's wrong time decrements by 10
+// CHECK ANSWER and SCORE Function - Listens for the answer event and compares it to the correct answer if it's correct score increase by 1 if it's wrong time decrements by 10
 function checkAnswer(answer) {
   if (answer === questionsArray[next].correct) {
     score++;
@@ -160,18 +152,18 @@ function checkAnswer(answer) {
   } else {
     secondsLeft -= 10;
   }
-  
 }
 
-// ENDSCREEN FUNCTION - Hide questions and timer while displaying endscreen and score div
+// ENDSCREEN and PULL user name and user score from local storage
 function endScreen() {
-
   document.getElementById("questionDisplay").style.visibility = "hidden";
   document.getElementById("endScreen").style.visibility = "visible";
   document.getElementById("score-div").style.visibility = "hidden";
   document.getElementById("timer-div").style.visibility = "hidden";
-  document.getElementById("name-display").textContent = localStorage.getItem("user");
-  document.getElementById("final-score").textContent = localStorage.getItem("userScore");
+  document.getElementById("name-display").textContent = localStorage.getItem(
+    "user"
+  );
+  document.getElementById("final-score").textContent = localStorage.getItem(
+    "userScore"
+  );
 }
-
-
